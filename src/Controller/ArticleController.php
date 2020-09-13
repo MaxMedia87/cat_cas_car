8e8995b49b7bcb81bdc31ddc29f6bb8af14e2ca8
Maksim Sakharov <maksim_saharov@mail.ru>
2020-09-13 20:51:45 +0300

    Подключение шаблонизатора twig. Создание тестового шаблона

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,9 +23,16 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Будущая страница статьи: %s',
-            ucwords(str_ireplace('-', ' ', $slug))
-        ));
+
+        $comments = [
+            'Не обращайте внимание на троллей.',
+            'Это глубоко несчастные люди. Они считают, что обижая других, могут хоть как-то сами возвыситься',
+            'Банить хамов надо, Вы полностью правы',
+            'Меня все устраивает. Не было желания после прочтения оставлять плохой комментарий. Удачи!'
+        ];
+        return $this->render('articles/show.html.twig', [
+            'article' => ucwords(str_ireplace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 }
